7849f410292b0340c2fc0861041d750dd48c1e0f
Faith Oseghale <ikahunuagbon0110@gmail.com>
2023-07-16 22:08:44 +0100

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/ABout me.jpg";
+import image from "../images/About.jpg";
 
 const imageAltText = "A shelf with books";
 
