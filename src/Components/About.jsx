14018dddaf64a4e2e037d5fa42e94f53c958a9c7
Oseghale Faith <96834683+Omonigho24@users.noreply.github.com>
2023-07-31 11:40:21 +0100

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/About.jpg";
+import image from "../images/about.png";
 
 const imageAltText = "A shelf with books";
 
@@ -24,19 +24,19 @@ const imageAltText = "A shelf with books";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Data Analyst with a passion for unravelling complex information, translating it into actionable insights, and empowering informed decision-making.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Analysis",
+  "Technical Writing",
+  "Research",
+  "Power BI",
+  "Python",
+  "Community lover",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my data analysis experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
